8d346f68c0b4f97505469c9aadbbbc47ce84508c
Brianna Shade <jarvis.brianna@gmail.com>
2013-07-05 10:28:05 -0700

    Simplified down to just a simple google search box.  Currently doesn't do anything when search is entered.

--- popup.js
+++ popup.js
@@ -12,38 +12,41 @@
 
 var query = 'kittens';
 
-var kittenGenerator = {		
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(query) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
+popupAction() {
+	var cx = '018373880543266522285:-8rnpoqzrd8';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		'//www.google.com/cse/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+};
 
+		/**
+		searchOnGoogle_: 'https://www.googleapis.com/customsearch/v1?' +
+	      'method=flickr.photos.search&' +
+	      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
+	      'text=' + encodeURIComponent(query) + '&' +
+	      'safe_search=1&' +
+	      'content_type=1&' +
+	      'sort=interestingness-desc&' +
+	      'per_page=20',
+		**/
   /**
    * Sends an XHR GET request to grab photos of lots and lots of kittens. The
    * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
    *
    * @public
    */
-  requestKittens: function() {
+/**
+requestKittens: function() {
     var req = new XMLHttpRequest();
     req.open("GET", this.searchOnFlickr_, true);
     req.onload = this.showPhotos_.bind(this);
     req.send(null);
   },
-
+**/
   /**
    * Handle the 'onload' event of our kitten XHR request, generated in
    * 'requestKittens', by generating 'img' elements, and stuffing them into
@@ -52,6 +55,7 @@ var kittenGenerator = {
    * @param {ProgressEvent} e The XHR ProgressEvent.
    * @private
    */
+/**
   showPhotos_: function (e) {
     var kittens = e.target.responseXML.querySelectorAll('photo');
     for (var i = 0; i < kittens.length; i++) {
@@ -61,7 +65,7 @@ var kittenGenerator = {
       document.body.appendChild(img);
     }
   },
-
+**/
   /**
    * Given a photo, construct a URL using the method outlined at
    * http://www.flickr.com/services/api/misc.urlKittenl
@@ -70,6 +74,7 @@ var kittenGenerator = {
    * @return {string} The kitten's URL.
    * @private
    */
+/**
   constructKittenURL_: function (photo) {
     return "http://farm" + photo.getAttribute("farm") +
         ".static.flickr.com/" + photo.getAttribute("server") +
@@ -77,9 +82,10 @@ var kittenGenerator = {
         "_" + photo.getAttribute("secret") +
         "_s.jpg";
   }
-};
+  **/
+
 
 // Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
+	popupAction();
 });
