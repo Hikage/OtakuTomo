c05028ddd3238dcd82bfdea7bbb43ab49f83f286
Brianna Shade <jarvis.brianna@gmail.com>
2013-07-31 16:04:34 -0700

    Have a dynamic HTML page.  Working to piece together results from a search.

--- popup.js
+++ popup.js
@@ -18,11 +18,20 @@ function getResults(searchURL){
 	xhr.overrideMimeType('text/xml');
 	xhr.send(null);
 	
+	var htmlHead = "<head><title>OtakuTomo Search Results</title></head>";
+	
 	var root = xhr.responseXML;
-	chrome.tabs.create({url: chrome.extension.getURL('results.html')});
-	var pics = root.getElementsByTagName("image");
-	alert("Got here!");
-	chrome.tabs.create({url: 'http://cdn.myanimelist.net/images/anime/6/7632.jpg'});
+	var results = root;
+	var htmlBody = "<body>" + results + "</body>";
+	
+	var htmlCode = "<html>" + htmlHead + htmlBody + "</html>";
+	var url = "data:text/html," + encodeURIComponent(htmlCode);
+	
+	chrome.tabs.create({url: url});
+	
+	//chrome.tabs.create({url: chrome.extension.getURL('results.html')});
+	//var pics = root.getElementsByTagName("image");
+	//chrome.tabs.create({url: 'http://cdn.myanimelist.net/images/anime/6/7632.jpg'});
 }
 
 function clickHandler(){
